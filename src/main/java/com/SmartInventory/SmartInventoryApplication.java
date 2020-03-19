0c277827059623701c19e6ffec8b187255cd3344
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2020-03-19 18:56:32 -0300

    Correção de nome de pacote

--- src/main/java/com/SmartInventory/SmartInventoryApplication.java
+++ src/main/java/com/SmartInventory/SmartInventoryApplication.java
@@ -1,4 +1,4 @@
-package c.SmartInventory;
+package com.SmartInventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/SmartInventory/SmartInventoryApplicationTests.java
+++ src/test/java/com/SmartInventory/SmartInventoryApplicationTests.java
@@ -1,4 +1,4 @@
-package c.SmartInventory;
+package com.SmartInventory;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
